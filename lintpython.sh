9b10d2fccc07ffb4d0470d8bd2ef0686ae1f2599
CHIKHAOUI-Firas99 <firas.chikhaoui@edu.isetcom.tn>
2023-07-06 13:12:06 +0000

    hello path

--- lintpython.sh
+++ lintpython.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export PATH=$HOME/.local/bin:$PATH
-pip install --upgrade pip
-pip install -r requirements.txt
-pylint .
+pip install --no-cache-dir --upgrade -r /requirements.txt
+pip install python-jose
+pip install python-dotenv
+pylint main.py
 
